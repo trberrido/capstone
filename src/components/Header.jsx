44a9ab0f4f5f8b2b240c6104e1581261b0ab7934
thierry <thibault@dafox.co>
2023-03-16 02:47:32 +0100

    put button around logo in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,8 @@
 import logo from '../assets/Logo.svg'
-
+import { Link } from 'react-router-dom';
 const Header = ({children}) => {
 	return (<header className='content-column'>
-		<img alt='' src={logo} />
+		<Link to='/'><img alt='' src={logo} /></Link>
 		{children}
 	</header>)
 }
